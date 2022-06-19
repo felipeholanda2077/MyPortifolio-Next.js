bf9ac93c9ec53f399fe69ff697434f3c4361617d
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-06-19 17:16:55 -0300

    Alteração de imagens

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -4,8 +4,8 @@ import Link from '../../components/Link'
 import { theme } from '../../theme/theme';
 import { Image, Box, Text, Icon, Input, Button } from '../../theme/components';
 
-const LOGO_ALURA_URL = '/images/alura-logo.svg';
-const SIDE_IMAGE_URL = '/images/alura-cases.png';
+const LOGO_ALURA_URL = '/images/Felipe.png';
+const SIDE_IMAGE_URL = '/images/Perfil.png';
 
 function SideImage() {
   return (
